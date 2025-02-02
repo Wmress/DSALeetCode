9459567738010084f3435c5915c963cfbe8ffdde
Wmress <ewolde2@huskers.unl.edu>
2025-02-02 09:24:39 -0600

    Fixed append functions

--- LinkedList.py
+++ LinkedList.py
@@ -10,29 +10,33 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-        def print_list(self):
-            #set a temp var to the head of the list
-            temp = self.head
-            #if temp has a value then run this loop
-            while temp is not None:
-                #print the value stored in temp
-                print(temp.value)
-                #set temp to the next node
-                temp = temp.next
+    def print_list(self):
+        #set a temp var to the head of the list
+        temp = self.head
+        #if temp has a value then run this loop
+        while temp is not None:
+            #print the value stored in temp
+            print(temp.value)
+            #set temp to the next node
+            temp = temp.next
 
-        def append(self, value):
-            new_node = Node(value)
-            temp = self.head
-            if self.head == None:
-                self.head = new_node
-                self.tail = new_node
-            else:
-                self.tail.next = new_node
-                self.tail = new_node
-            self.lenght += 1
-            return True
-            
+    def append(self, value):
+        #create a new node with the value stored in the node
+        new_node = Node(value)
+        #if there is no node in the list
+        if self.head == None:
+            #then set the head and tail to the new node
+            self.head = new_node
+            self.tail = new_node
+        else:
+            #add the node to the end of the list
+            self.tail.next = new_node
+            self.tail = new_node
+        #expand the length by one
+        self.length += 1
 
-my_linked_list = LinkedList(4)
+my_linked_list = LinkedList(1)    
 
-print(my_linked_list)
+my_linked_list.append(2)
+
+my_linked_list.print_list()
